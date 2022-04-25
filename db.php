5686650ce5d1aa87df04fd6592a46b9ec1863b3a
annisbalqisa <202410101045@mail.unej.ac.id>
2022-04-25 23:33:14 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'uts_202410101045';
+    $DATABASE_USER = '202410101045';
+    $DATABASE_PASS = 'secret';
+    $DATABASE_NAME = 'uas202410101045';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
